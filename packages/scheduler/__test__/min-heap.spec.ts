64c9b70779297227c1dff610ef8e83df924a152b
proc07 <790847400@qq.com>
2024-12-24 10:00:01 +0800

    feat: init project

---
+++ packages/scheduler/__test__/min-heap.spec.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest'
+
+function createNode(val: number) {
+  return { sortIndex: val }
+}
+
+describe('test min heap', () => {
+  it('empty heap case', () => {
+    const tasks = []
+  })
+  it('min heap length === 1 case', () => {
+
+  })
+  it('min heap case', () => {
+
+  })
+})
